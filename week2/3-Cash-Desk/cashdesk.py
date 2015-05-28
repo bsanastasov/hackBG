eb075239ecd3c106f2764c0d5d3b7f8925aa323a
bsanastasov <bs.anastasov@gmail.com>
2015-05-28 12:28:38 +0300

    Update cashdesk.py

--- week2/3-Cash-Desk/cashdesk.py
+++ week2/3-Cash-Desk/cashdesk.py
@@ -1,41 +1,5 @@
-class Bill:
-
-	def __init__(self,amount):
-		self.amount = amount
-
-	def __str__(self):
-		return "A {} is a bill".format(self.amount)
-
-	def __repr__(self):
-		return self.__str__()
-
-	def __int__(self):
-		return self.amount
-
-	def __eq__(self, other):
-		return self.amount == other.amount
-
-	def __hash__(self):
-		return hash(self.amount)
-
-class BachBill(Bill):
-	def __init__(self,bills):
-		self.bills = bills
-
-	def __len__(self):
-		return len(self.bills)
-
-	def total(self):
-		sum = 0
-		for bill in self.bills:
-			sum += int(bill)
-		return sum
-
-	def __getitem__(self, index):
-		return self.bills[index]
-
-	def __int__(self):
-		return self.total()
+from bill import Bill
+from batch_bill import BatchBill
 
 class CashDesk:
 	def __init__(self):
